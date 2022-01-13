7b4318c981a5bfd7083992e34bf9f9e8bcf88f43
Dirk van Krieken <dvankrieken@protonmail.com>
2022-01-13 14:20:23 +0100

    add if statements

--- srcs/free.c
+++ srcs/free.c
@@ -6,7 +6,7 @@
 /*   By: dvan-kri <dvan-kri@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/03 15:58:54 by dvan-kri      #+#    #+#                 */
-/*   Updated: 2022/01/03 15:58:55 by dvan-kri      ########   odam.nl         */
+/*   Updated: 2022/01/12 17:09:25 by dvan-kri      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ void	free_cmd1_options_and_exit(char **cmd1_options)
 		free(cmd1_options[i]);
 		i++;
 	}
+	if (cmd1_options)
+		free(cmd1_options);
 	exit(1);
 }
 
@@ -36,12 +38,16 @@ void	free_cmd12optionscmd1(t_data *data)
 		free(data->cmd1_options[i]);
 		i++;
 	}
+	if (data->cmd1_options)
+		free(data->cmd1_options);
 	i = 0;
 	while (data->cmd2_options[i] != NULL)
 	{
 		free(data->cmd2_options[i]);
 		i++;
 	}
+	if (data->cmd2_options)
+		free(data->cmd2_options);
 	if (data->cmd1)
 		free(data->cmd1);
 }
@@ -56,12 +62,16 @@ void	free_all(t_data *data)
 		free(data->cmd1_options[i]);
 		i++;
 	}
+	if (data->cmd1_options)
+		free(data->cmd1_options);
 	i = 0;
 	while (data->cmd2_options[i] != NULL)
 	{
 		free(data->cmd2_options[i]);
 		i++;
 	}
+	if (data->cmd2_options)
+		free(data->cmd2_options);
 	if (data->cmd1)
 		free(data->cmd1);
 	if (data->cmd2)
